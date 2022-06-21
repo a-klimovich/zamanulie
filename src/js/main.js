fde4c783f1e94226629b69a0610ee9978cb729b3
a-klimovich <ar2r.klimovich@gmail.com>
2022-06-21 23:33:08 +0200

    init

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,26 @@
     });
   });
 
+  // MODAL
+  const modals = document.querySelectorAll('[data-modal]');
+
+  modals.forEach(function (trigger) {
+    trigger.addEventListener('click', function (event) {
+      event.preventDefault();
+      const modal = document.getElementById(trigger.dataset.modal);
+      const exits = modal.querySelectorAll('.modal-exit');
+
+      modal.classList.add('open');
+
+      exits.forEach(function (exit) {
+        exit.addEventListener('click', function (event) {
+          event.preventDefault();
+          modal.classList.remove('open');
+        });
+      });
+    });
+  });
+
 })();
 
 
