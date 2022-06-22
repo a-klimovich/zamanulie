518e001328e8d5aeaa3c1dbe8ef07ec36b360029
a-klimovich <ar2r.klimovich@gmail.com>
2022-06-22 22:57:20 +0200

    asv

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,54 @@
 (function () {
   $(document).ready(function () {
-    $(".your-class").slick({
-    });
+    $(".your-class").slick({});
+  });
+
+  // HEADER
+  const burgerBtn = $(".burger");
+  const nav = $(".nav");
+  const header = $(".header");
+  const headerDecor = $(".header__decor");
+  let scrollPrev = 0;
+
+  // open nav
+  burgerBtn.on('click', () => {
+    nav.toggleClass('active');
+    burgerBtn.toggleClass('burger--active');
+    headerDecor.toggleClass('is-hidden');
+  });
+
+  // hede show menu on scroll
+  $(window).scroll(function () {
+    const scrolled = $(window).scrollTop();
+
+    if (scrolled > 71 && scrolled > scrollPrev) {
+      header.addClass("out");
+      header.addClass("scroll");
+    } else {
+      header.removeClass("out");
+      header.removeClass("scroll");
+    }
+
+    scrollPrev = scrolled;
   });
 
   // MODAL
-  const modals = document.querySelectorAll('[data-modal]');
+  const modals = document.querySelectorAll("[data-modal]");
 
   modals.forEach(function (trigger) {
-    trigger.addEventListener('click', function (event) {
+    trigger.addEventListener("click", function (event) {
       event.preventDefault();
       const modal = document.getElementById(trigger.dataset.modal);
-      const exits = modal.querySelectorAll('.modal-exit');
+      const exits = modal.querySelectorAll(".modal-exit");
 
-      modal.classList.add('open');
+      modal.classList.add("open");
 
       exits.forEach(function (exit) {
-        exit.addEventListener('click', function (event) {
+        exit.addEventListener("click", function (event) {
           event.preventDefault();
-          modal.classList.remove('open');
+          modal.classList.remove("open");
         });
       });
     });
   });
-
 })();
-
-
-
