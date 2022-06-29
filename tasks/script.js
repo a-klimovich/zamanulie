2b11632379924e627f968c7dc73b3b6e93aa4ffc
a-klimovich <ar2r.klimovich@gmail.com>
2022-06-29 19:51:23 +0200

    minify +

--- tasks/script.js
+++ tasks/script.js
@@ -1,16 +1,19 @@
 const { src, dest } = require("gulp");
 const path = require('./config/path.js');
-// const processes = require('./config/processes.js');
+const processes = require('./config/processes.js');
 
 // PLAGINS
-// const gulpIf = require('gulp-if');
+const gulpIf = require('gulp-if');
 const concat = require('gulp-concat');
 const babel = require('gulp-babel')
+const minify = require('gulp-minify');
+ 
 
 const script = () => {
   return src(path.script.src)
     .pipe(babel())
     .pipe(concat('index.js'))
+    .pipe(gulpIf(processes.isProd, minify()))
     .pipe(dest(path.script.build))
 };
 
